5cf75b02525d6d9e445ce7a30075a44988cbe9d8
chiragsingh4560 <120973823+chiragsingh4560@users.noreply.github.com>
2025-02-18 12:06:49 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,17 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AdminContextProvider, { AdminContext } from "./context/AdminContext.jsx";
+import AdminContextProvider from "./context/AdminContext.jsx"; // Correct import
 import DoctorContextProvider from "./context/DoctorContext.jsx";
-import AppContextProvider from "./context/AdminContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx"; // Fixed this import
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AdminContextProvider>
-      <DoctorContextProvider>
-        <AppContextProvider>
-          <App />
-        </AppContextProvider>
-      </DoctorContextProvider>
-    </AdminContextProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <AppContextProvider>
+        {" "}
+        {/* AppContextProvider should wrap everything */}
+        <AdminContextProvider>
+          <DoctorContextProvider>
+            <App />
+          </DoctorContextProvider>
+        </AdminContextProvider>
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
